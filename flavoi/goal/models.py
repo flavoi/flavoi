dff44dfc95efc742359fc38470ba1fc046b825b4
flavoi <flavius476@gmail.com>
2015-12-08 20:45:33 +0000

    Updated font-awesome cdn, added abstract attributo to achievements.

--- flavoi/goal/models.py
+++ flavoi/goal/models.py
@@ -36,6 +36,7 @@ class Goal(TimeStampedModel):
         To add: work in progress details.
     """
     title = models.CharField(max_length=60)
+    abstract = models.TextField(blank=True)
     description = models.TextField(blank=True, null=True)
     picture = models.ImageField(upload_to=settings.MEDIA_ROOT+'goals/', blank=True, null=True)
     published = models.BooleanField(default=False)
