2e8b567013042d25f8ce2d7640801ccc43eaf011
Flavio Marcato <flavius476@gmail.com>
2017-07-22 15:56:09 +0200

    Adjusted font size and weight and removed navigaion bar from goal details' page

--- flavoi/bio/admin.py
+++ flavoi/bio/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from bio.models import Bio, Contact, Inspiration, Momenti
 
 class BioAdmin(admin.ModelAdmin):
-    list_display = ['title', 'name', 'surname', 'active']
+    list_display = ['name', 'active']
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['label', 'link', 'primary']
