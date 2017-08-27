6d18d8ded1653f4b64259176a56d1b34f68a0566
Flavio Marcato <flavius476@gmail.com>
2017-08-27 16:23:48 +0200

    Added seaearch input in navbar.

--- flavoi/bio/urls.py
+++ flavoi/bio/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from .views import GreetingView, HomeView, InspirationsView, ContactsView, MomentiView
 
 urlpatterns = [
-    # url(r'^hello/$', GreetingView.as_view()),
-    url(r'^$', HomeView.as_view(), name='about'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^about/$', HomeView.as_view(), name='about'),
     url(r'^inspirations/$', InspirationsView.as_view(), name='inspirations'),
     # url(r'^contacts/$', ContactsView.as_view(), name='contacts'),
     # url(r'^momenti/$', MomentiView.as_view(), name='momenti'),
